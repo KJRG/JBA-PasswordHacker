8e0011ba42539bc4db12a602d7821123f2e0e556
KJRG <kjrgalka@gmail.com>
2021-10-27 21:39:36 +0200

    Added tasks from the Conversion to boolean topic

---
+++ Topics/Conversion to boolean/Say hello/main.py
@@ -0,0 +1,3 @@
+def say_hello(name):
+    name_to_print = name or "Anonymous"
+    print(f"Hello, {name_to_print}!")
---
+++ Topics/Conversion to boolean/XOR operator/main.py
@@ -0,0 +1,5 @@
+def xor(a, b):
+    # Write your code here
+    if bool(a) == bool(b):
+        return False
+    return a or b
